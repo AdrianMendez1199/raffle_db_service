df6db5b4b038d2607810e304f7097a7a1658da7e
Adrian Mendez <mendezadrian149@gmail.com>
2020-07-01 21:39:34 -0400

    update table users with select winner

--- src/services/user/service.ts
+++ src/services/user/service.ts
@@ -4,9 +4,11 @@ import db from '../../../database';
 import { GRPC_HOST_USER, PROTO_PATH_USER } from '../const';
 
 export interface User {
+  id?: number;
   name: string;
   identificationCard: string;
   code: number;
+  winner?: number;
 }
 
 /**
@@ -32,10 +34,15 @@ async function createUser(ctx: Context): Promise<User> {
  */
 async function getWinner(ctx: Context): Promise<User> {
   const winner = await db
-  .select('name', 'identificationCard', 'code')
+  .select('id', 'name', 'code', 'identificationCard')
   .from<User>('users')
+  .where({ winner: 0 })
   .orderByRaw('RANDOM()')
   .limit(1);
+
+  await db<User>('users')
+  .where({ id: winner[0].id })
+  .update({ winner: 1 });
 
   return ctx.res = { ...winner[0] };
 }
