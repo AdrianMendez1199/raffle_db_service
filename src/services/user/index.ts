28c979136ec87c5f25b04c600360afcf6c70416b
Adrian Mendez <mendezadrian149@gmail.com>
2020-07-02 21:09:37 -0400

    exporting gRPC client like a promises

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -1,9 +1,8 @@
 import { loadPackageDefinition, credentials } from 'grpc';
 import { loadSync } from '@grpc/proto-loader';
-import { promisify } from 'util';
 
 import { GRPC_HOST_USER, PROTO_PATH_USER  } from '../const';
-
+import { convertCallbackClientToPromise } from '../../../utils';
 
 const proto = loadSync(PROTO_PATH_USER, {
   keepCase: true,
@@ -15,22 +14,5 @@ const userServiceClient: any = loadPackageDefinition(proto).user;
 const UserService = userServiceClient.UserService;
 const userClient = new UserService(GRPC_HOST_USER, credentials.createInsecure());
 
-
-// TO REVIEW
-const promises: any = {};
-const functionsList = Object.keys(Object.getPrototypeOf(userClient));
-
-
-//// TO REVIEW
-const fn = functionsList.filter((functions: string) => {
-  if (!functions.startsWith('$')) {
-    return functions;
-  }
-});
-
-fn.map((i: any) => {
-  promises[i] = promisify(userClient[i]).bind(userClient);
-  return Object.assign({}, promises);
-});
-
-export default promises;
+// Export userClient convert in Promise
+export default convertCallbackClientToPromise(userClient);
