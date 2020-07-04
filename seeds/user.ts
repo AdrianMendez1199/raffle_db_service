572bcc7cce3e6baa60a8cd163f1d7e59d7e8046a
Adrian Mendez <mendezadrian149@gmail.com>
2020-07-04 10:41:02 -0400

    replace doble quotes for simples

--- seeds/user.ts
+++ seeds/user.ts
@@ -1,14 +1,14 @@
-import * as Knex from "knex";
+import * as Knex from 'knex';
 
 export async function seed(knex: Knex): Promise<any> {
   // Deletes ALL existing entries
-  return knex("users").del()
+  return knex('users').del()
     .then(() => {
       // Inserts seed entries
-      return knex("users").insert([
-        { name: "Adrian", identificationCard: "40211031212", code: 1234 },
-        { name: "Wilo", identificationCard: "40211031211", code: 1235 },
-        { name: "Raymon", identificationCard: "40211031215", code: 1238 },
+      return knex('users').insert([
+        { name: 'Adrian', identificationCard: '40211031212', code: 1234 },
+        { name: 'Wilo', identificationCard: '40211031211', code: 1235 },
+        { name: 'Raymon', identificationCard: '40211031215', code: 1238 },
       ]);
     });
 }
